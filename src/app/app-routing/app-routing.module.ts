e7dc35ba9b0d789251328929448a18178ede657c
zy823957866 <823957866@qq.com>
2018-10-07 15:28:27 +0800

    路由修改

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from '../login/login.component';
+import { LoginComponent } from '../app-views/login/login.component';
 
 
 //路由设置
@@ -24,6 +24,8 @@ const routes:Routes = [
  	imports: [
 		RouterModule.forRoot(routes)
   	],
-  	declarations: []
+  	declarations: [
+		LoginComponent
+	  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
