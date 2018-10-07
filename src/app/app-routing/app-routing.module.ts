2ed3b89318d5db604250800b22f289d081657b7a
zy823957866 <823957866@qq.com>
2018-10-07 13:42:01 +0800

    创建项目

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+
+
+//路由设置
+const routes:Routes = [
+	{path: '', redirectTo: '/login', pathMatch: 'full'},
+	{path: '*', redirectTo: '/login',pathMatch: 'full'},
+	{path: 'login', component: LoginComponent},
+	{
+		path: 'sys', 
+		component: LoginComponent,
+		children: [
+
+		]
+	}
+];
+
+@NgModule({
+	exports:[
+    	RouterModule
+  	],
+ 	imports: [
+		RouterModule.forRoot(routes)
+  	],
+  	declarations: []
+})
+export class AppRoutingModule { }
